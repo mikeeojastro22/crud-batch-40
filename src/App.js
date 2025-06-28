81993793d489d920a32f80dc2e228b186b13e006
Mikee Ojastro <mikeeojastro@gmail.com>
2025-06-28 10:49:44 +0800

    June 28, 2025

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
+import { useState } from 'react';
 import './App.css';
+import data from './assets/task-list.json';
+import Task from './components/Task.jsx';
 
 function App() {
+
+  const [tasks, setTasks] = useState(data); // data is an array
+  const [count, setCount] = useState(tasks.length);
+
+  const handleAddTask = (newTask) => {
+    // newTask = user input which will come from the Task component
+
+    // [...prevTasks, newTask] - ... - spread operator - creates a new array that contains the previous tasks with the newly created task
+    setTasks((prevTasks) => [...prevTasks, newTask]);
+
+    // updating the next id
+    const newCount = count + 1;
+    setCount(newCount);
+  }
+
+  // Delete
+  const handleDeleteTask = (taskId) => {
+    // filter - matches the condition then it will return the element if the condition is true; creates new array; traverses through the array
+    // setTasks - function binded to tasks
+    // prevTasks - current tasks
+    // task - pertains to each element of tasks
+    // we only include the non deleted tasks in our new array/excluding the task id that the user wants to delete
+    setTasks((prevTasks) => prevTasks.filter((task) => task.id !== taskId));
+  }
+
   return (
     <div className="App">
-      
+      {/* Read */}
+      {
+        tasks.map((task) => {
+          return (
+            <div key={task.id}>
+              <span>{task.task_name}</span>
+              <button onClick={() => handleDeleteTask(task.id)}>Delete</button>
+            </div>
+          )
+        })
+      }
+      <Task handleAddTask={handleAddTask} newId={count} />
     </div>
   );
 }
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,7 +1,32 @@
+import { useState } from "react";
 
-function Task() {
+function Task({handleAddTask, newId}) {
+  const [toDo, setToDo] = useState("");
+
+  const addTaskHandler = (event) => {
+    // prevents the page from reloading
+    event.preventDefault();
+
+    const newTaskObject = {
+      task_name: toDo,
+      id: newId
+    }
+
+    handleAddTask(newTaskObject);
+    setToDo('');
+  }
+
   return (
-    <div>Task</div>
+    <div>
+      <form onSubmit={addTaskHandler}>
+        <input
+          value={toDo}
+          placeholder="Add task here"
+          onChange={(event) => setToDo(event.target.value)}
+        ></input>
+        <button>Add Task</button>
+      </form>
+    </div>
   );
 }
 
